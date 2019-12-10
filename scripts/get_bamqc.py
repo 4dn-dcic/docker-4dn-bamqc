f348b50e7ae1447522899e670be6f65c7d071861
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2019-12-10 14:46:24 -0500

    Updated get_bamqc script

--- scripts/get_bamqc.py
+++ scripts/get_bamqc.py
@@ -2,7 +2,6 @@ import pysam
 import json
 import click
 import collections
-import utilities
 
 
 @click.command()
@@ -18,37 +17,117 @@ def main(infile, chromsizes, outdir, filename):
         chrmsizes = opf.readlines()
 
     prev_read_id = None
-    pairs_tags_dict = collections.OrderedDict()
-    pairs_tags_dict['Minor Contigs'] = 0
+    pairs_tags_count_dict = collections.OrderedDict()
+    pairs_tags_count_dict['Minor Contigs'] = 0
     pairs_tag_list = []
-    main_chroms = [x for x in range(len(chrmsizes))] # the chromsizes file
-    candidates = []
+    main_chroms = [x for x in range(len(chrmsizes))]  # the chromsizes file
+    lines_same_id = []
 
-    for read in bamfile:
-        candidate_info = {'id': read.query_name,
-                          'chrm': read.reference_id,
-                          'pos': read.pos,
-                          'chrm_next': read.next_reference_id,
-                          'pos_next': read.pnext,
-                          'tag': read.get_tag('Yt'),
-                          'chrm_pos': (read.reference_id, read.pos),
-                          'chrm_pos_next': (read.next_reference_id, read.pnext)}
+    for bamline in bamfile:
+        # Getting bamline information
+        bamline_info = {'id': bamline.query_name,
+                        'chrm': bamline.reference_id,
+                        'tag': bamline.get_tag('Yt'),
+                        'secondary_alignment': bamline.is_secondary}
 
-        if candidate_info['tag'] not in pairs_tags_dict.keys():
-            pairs_tags_dict[candidate_info['tag']] = 0
+        if bamline_info['tag'] not in pairs_tags_count_dict.keys():
+            pairs_tags_count_dict[bamline_info['tag']] = 0
 
-        if prev_read_id is not None and read.query_name != prev_read_id:
-            # This is a group of lines with the same id. Send to process
-            utilities.process_read_ids_block(candidates, pairs_tag_list, pairs_tags_dict, main_chroms)
+        if prev_read_id is not None and bamline.query_name != prev_read_id:
+            # A line with a new read_id is being reached
+            # Group lines with the previous read_id into reads and count number of reads per tag.
+            lines_per_tag_groups = []  # groupig lines per tags
+            for tag in set(pairs_tag_list):
+                lines_per_tag_group = []
+                for line_same_id in lines_same_id:
+                    if line_same_id['tag'] == tag:
+                        lines_per_tag_group.append(line_same_id)
+                lines_per_tag_groups.append(lines_per_tag_group)
+
+            for tag_group in lines_per_tag_groups:  # Processing each group of tags
+                read_num = 0
+                track_minor_contigs = False
+                subset_value = 0
+                if tag_group[0]['tag'] == "UU":  # count minor contigs for this flag type
+                    track_minor_contigs = True
+                    subset_counter = 0
+                    subset_value = 2  # each UU read has only 2 lines, use this info to keep track of minor contigs
+                    is_minor_contig = False
+                elif (tag_group[0]['tag'] == "RU" or tag_group[0]['tag'] == "UR"):  # count minor contigs for this flag type
+                    track_minor_contigs = True
+                    subset_value = 3  # each RU and UR has only 3 lines, use this info to keep track of minor contigs
+                    subset_counter = 0
+                    is_minor_contig = False
+
+                for i, a_line in enumerate(tag_group):
+                    if not a_line['secondary_alignment']:
+                        read_num = read_num + 1
+
+                    # for UU, UR and RU flags check if a line within a read is mapped to a non main chromosome,
+                    # this count will be add to minor contigs count and removed from filtered reads count
+                    if track_minor_contigs:
+                        subset_counter = subset_value
+
+                        if a_line['chrm'] not in main_chroms:
+                            is_minor_contig = True
+
+                        if (i + 1) == subset_counter:
+                            if is_minor_contig:
+                                pairs_tags_count_dict['Minor Contigs'] = pairs_tags_count_dict['Minor Contigs'] + 1
+                                is_minor_contig = False
+                            subset_counter = subset_counter + subset_value
+
+                num_reads_tag_group = read_num // 2
+                pairs_tags_count_dict[tag_group[0]['tag']] = pairs_tags_count_dict[tag_group[0]['tag']] + num_reads_tag_group
+
             pairs_tag_list = []
-            candidates = []
+            lines_same_id = []
 
-        pairs_tag_list.append(candidate_info['tag'])
-        prev_read_id = read.query_name
-        candidates.append(candidate_info)
+        pairs_tag_list.append(bamline_info['tag'])
+        prev_read_id = bamline.query_name
+        lines_same_id.append(bamline_info)
 
-    if len(pairs_tag_list) != 0:
-        utilities.process_read_ids_block(candidates, pairs_tag_list, pairs_tags_dict, main_chroms)
+    if len(pairs_tag_list) != 0:  # There are some lines left to process, repeat processing
+        lines_per_tag_groups = []
+        for tag in set(pairs_tag_list):
+            lines_per_tag_group = []
+            for line_same_id in lines_same_id:
+                if line_same_id['tag'] == tag:
+                    lines_per_tag_group.append(line_same_id)
+            lines_per_tag_groups.append(lines_per_tag_group)
+
+        for tag_group in lines_per_tag_groups:
+            read_num = 0
+            track_minor_contigs = False
+            subset_value = 0
+            if tag_group[0]['tag'] == "UU":
+                track_minor_contigs = True
+                subset_counter = 0
+                subset_value = 2
+                is_minor_contig = False
+            elif (tag_group[0]['tag'] == "RU" or tag_group[0]['tag'] == "UR"):
+                track_minor_contigs = True
+                subset_value = 3
+                subset_counter = 0
+                is_minor_contig = False
+
+            for i, a_line in enumerate(tag_group):
+                if not a_line['secondary_alignment']:
+                    read_num = read_num + 1
+                if track_minor_contigs:
+                    subset_counter = subset_value
+
+                    if a_line['chrm'] not in main_chroms:
+                        is_minor_contig = True
+
+                    if (i + 1) == subset_counter:
+                        if is_minor_contig:
+                            pairs_tags_count_dict['Minor Contigs'] = pairs_tags_count_dict['Minor Contigs'] + 1
+                            is_minor_contig = False
+                        subset_counter = subset_counter + subset_value
+
+            num_reads_tag_group = read_num // 2
+            pairs_tags_count_dict[tag_group[0]['tag']] = pairs_tags_count_dict[tag_group[0]['tag']] + num_reads_tag_group
 
     # QC Report
     report_dict = collections.OrderedDict()
@@ -59,7 +138,7 @@ def main(infile, chromsizes, outdir, filename):
     duplicates = 0
     unique = 0
 
-    for key, val in pairs_tags_dict.items():
+    for key, val in pairs_tags_count_dict.items():
         if key != "Minor Contigs":
             total_reads = total_reads + val
         if key == "RU" or key == "UR":
@@ -73,7 +152,7 @@ def main(infile, chromsizes, outdir, filename):
         elif key == "UU":
             unique = val
 
-    filtered = unique + rescued_chimeras - pairs_tags_dict['Minor Contigs']
+    filtered = unique + rescued_chimeras - pairs_tags_count_dict['Minor Contigs']
 
     report_dict['Total Reads'] = total_reads
     report_dict['% of reads mapped'] = (1 - (unmmapped / total_reads)) * 100
@@ -81,7 +160,7 @@ def main(infile, chromsizes, outdir, filename):
     report_dict['% of reads filtered'] = filtered/total_reads * 100
     report_dict['% of rescued chimeras'] = rescued_chimeras/total_reads * 100
     report_dict['Filtered Reads'] = filtered
-    report_dict.update(pairs_tags_dict)
+    report_dict.update(pairs_tags_count_dict)
 
     with open(outpath, 'w') as outfile:
         json.dump(report_dict, outfile, indent=2)
