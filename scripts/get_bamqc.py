ec1934a39358fa27b3ad5d284912107576450a8d
Luisa Mercado <42242797+Lmercadom@users.noreply.github.com>
2020-01-10 10:34:33 -0500

    Added additional comments for code clarification

--- scripts/get_bamqc.py
+++ scripts/get_bamqc.py
@@ -18,7 +18,7 @@ def main(infile, chromsizes, outdir, filename):
 
     prev_read_id = None
     pairs_tags_count_dict = collections.OrderedDict()
-    pairs_tags_count_dict['Minor Contigs'] = 0
+    pairs_tags_count_dict['Minor Contigs'] = 0 # Keeps track of minor contigs for UU, RU and UR flag types
     pairs_tag_list = []
     main_chroms = [a_line.split('\t')[0] for a_line in chrmsizes]  # the chromsizes file
     lines_same_id = []
@@ -35,8 +35,8 @@ def main(infile, chromsizes, outdir, filename):
 
         if prev_read_id is not None and bamline.query_name != prev_read_id:
             # A line with a new read_id is being reached
-            # Group lines with the previous read_id into reads and count number of reads per tag.
-            lines_per_tag_groups = []  # groupig lines per tags
+            # Group lines with the previous read_id into reads and count number of reads per tag type.
+            lines_per_tag_groups = []  # grouping lines per tag type
             for tag in set(pairs_tag_list):
                 lines_per_tag_group = []
                 for line_same_id in lines_same_id:
@@ -44,27 +44,27 @@ def main(infile, chromsizes, outdir, filename):
                         lines_per_tag_group.append(line_same_id)
                 lines_per_tag_groups.append(lines_per_tag_group)
 
-            for tag_group in lines_per_tag_groups:  # Processing each group of tags
+            for tag_group in lines_per_tag_groups:  # Processing each group of tag types
                 read_num = 0
                 track_minor_contigs = False
-                subset_value = 0
+                subset_value = 0 # how many lines correspond to a single read for each type of flag, use this info to keep track of minor contigs
                 if tag_group[0]['tag'] == "UU":  # count minor contigs for this flag type
                     track_minor_contigs = True
                     subset_counter = 0
-                    subset_value = 2  # each UU read has only 2 lines, use this info to keep track of minor contigs
+                    subset_value = 2  # a read that has the UU flag is represented by 2 lines only. 
                     is_minor_contig = False
                 elif (tag_group[0]['tag'] == "RU" or tag_group[0]['tag'] == "UR"):  # count minor contigs for this flag type
                     track_minor_contigs = True
-                    subset_value = 3  # each RU and UR has only 3 lines, use this info to keep track of minor contigs
+                    subset_value = 3  #  a read that has the RU and UR flag is represented by 3 lines only.
                     subset_counter = 0
                     is_minor_contig = False
 
                 for i, a_line in enumerate(tag_group):
-                    if not a_line['secondary_alignment']:
+                    if not a_line['secondary_alignment']: # a read has only 2 primary alignments, the rest are secondary. 
                         read_num = read_num + 1
 
                     # for UU, UR and RU flags check if a line within a read is mapped to a non main chromosome,
-                    # this count will be add to minor contigs count and removed from filtered reads count
+                    # this count will be added to minor contigs count and removed from filtered reads count in the final output
                     if track_minor_contigs:
                         subset_counter = subset_value
 
@@ -77,7 +77,7 @@ def main(infile, chromsizes, outdir, filename):
                                 is_minor_contig = False
                             subset_counter = subset_counter + subset_value
 
-                num_reads_tag_group = read_num // 2
+                num_reads_tag_group = read_num // 2 # divide the number of primary alignments by 2 to obtain the number of reads per tag type
                 pairs_tags_count_dict[tag_group[0]['tag']] = pairs_tags_count_dict[tag_group[0]['tag']] + num_reads_tag_group
 
             pairs_tag_list = []
@@ -87,7 +87,7 @@ def main(infile, chromsizes, outdir, filename):
         prev_read_id = bamline.query_name
         lines_same_id.append(bamline_info)
 
-    if len(pairs_tag_list) != 0:  # There are some lines left to process, repeat processing
+    if len(pairs_tag_list) != 0:  # There are some lines left to process, repeat processing as above
         lines_per_tag_groups = []
         for tag in set(pairs_tag_list):
             lines_per_tag_group = []
@@ -138,7 +138,7 @@ def main(infile, chromsizes, outdir, filename):
     duplicates = 0
     unique = 0
 
-    for key, val in pairs_tags_count_dict.items():
+    for key, val in pairs_tags_count_dict.items(): # Categorize each flag type
         if key != "Minor Contigs":
             total_reads = total_reads + val
         if key == "RU" or key == "UR":
